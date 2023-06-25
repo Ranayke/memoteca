24be2c572dcd8c01c04e43ce9e790eb8aa98ed5f
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-06-24 21:53:12 -0300

    Created thought page component

---
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thought',
+  templateUrl: './create-thought.component.html',
+  styleUrls: ['./create-thought.component.css']
+})
+export class CreateThoughtComponent implements OnInit {
+
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {}
+
+  criarPensamento() {
+    alert('Novo pensamento criado')
+  }
+
+  cancelar() {
+    alert('Ação cancelada')
+  }
+
+}
