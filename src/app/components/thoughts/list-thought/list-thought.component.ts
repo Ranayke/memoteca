550cef789fedbf407743ecd93f124e86ff508095
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-06-26 14:28:45 -0300

    Created JSON server and pensamento interface

--- src/app/components/thoughts/list-thought/list-thought.component.ts
+++ src/app/components/thoughts/list-thought/list-thought.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-list-thought',
@@ -7,24 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListThoughtComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com o @Input()',
-      autoria: 'Componente filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-      autoria: 'NgClass',
-      modelo: 'modelo2'
-    }
-
-  ]
+  listaPensamentos: Pensamento[] = [];
 
   constructor() {}
 
--- src/app/components/thoughts/thought/thought.component.ts
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-thought',
@@ -7,7 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ThoughtComponent implements OnInit {
 
-  @Input() pensamento = {
+  @Input() pensamento: Pensamento = {
+    id: 0,
     conteudo: 'I love Angular',
     autoria: 'Ranayke',
     modelo: 'modelo3'
