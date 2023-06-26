7caeaab304bdc9f537a7df0ac1edccbc4c29f42a
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-06-26 15:41:03 -0300

    Created edit method in service file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 import { ListThoughtComponent } from './components/thoughts/list-thought/list-thought.component';
 import { DeleteThoughtComponent } from './components/thoughts/delete-thought/delete-thought.component';
+import { EditThoughtComponent } from './components/thoughts/edit-thought/edit-thought.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'thoughts/deleteThought/:id',
     component: DeleteThoughtComponent
+  },
+  {
+    path: 'thoughts/editThought/:id',
+    component: EditThoughtComponent
   }
 ];
 
