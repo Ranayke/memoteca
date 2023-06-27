0c6a41688b82ce314112f7b7bf30a7e521307945
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-06-27 09:59:37 -0300

    Created reactive forms and validators

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { ThoughtService } from '../thought.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-thought',
@@ -10,30 +11,47 @@ import { Router } from '@angular/router';
 })
 export class CreateThoughtComponent implements OnInit {
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
+  formulario!: FormGroup;
 
   constructor(
     private thoughtService: ThoughtService,
-    private router: Router
-  ) {
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
+      modelo: ['modelo1']
+    })
   }
 
-  ngOnInit(): void {}
-
   criarPensamento() {
-    this.thoughtService.criar(this.pensamento)
-      .subscribe(() => {
-        this.router.navigate(['/list-thought'])
-      })
+    if(this.formulario.valid) {
+      this.thoughtService.criar(this.formulario.value)
+        .subscribe(() => {
+          this.router.navigate(['/list-thought'])
+        })
+    }
   }
 
   cancelar() {
     this.router.navigate(['/list-thought'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
   }
 
 }
